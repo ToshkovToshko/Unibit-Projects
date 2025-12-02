bbf044aacde517e829ec14d37e453b1df2e8c3ac
Toshko Toshkov <toshkotoshkovtoshkov@gmail.com>
2025-12-02 10:33:59 +0200

    Add files via upload

---
+++ Car-Мaintenance/VSP_238sz_MyProject/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace VSP_238sz_MyProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBoxLastChange = new TextBox();
+            textBoxCurrentKm = new TextBox();
+            buttonCalculate = new Button();
+            buttonSave = new Button();
+            checkBoxOil = new CheckBox();
+            checkBoxGearbox = new CheckBox();
+            checkBoxTimingBelt = new CheckBox();
+            checkBoxPads = new CheckBox();
+            checkBoxBrakesDiscs = new CheckBox();
+            buttonClear = new Button();
+            richTextBoxOutput = new RichTextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(40, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Километри последна смяна:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(369, 19);
+            label2.Name = "label2";
+            label2.Size = new Size(143, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Текущи километри:";
+            // 
+            // textBoxLastChange
+            // 
+            textBoxLastChange.Location = new Point(40, 51);
+            textBoxLastChange.Name = "textBoxLastChange";
+            textBoxLastChange.Size = new Size(303, 27);
+            textBoxLastChange.TabIndex = 2;
+            // 
+            // textBoxCurrentKm
+            // 
+            textBoxCurrentKm.Location = new Point(369, 51);
+            textBoxCurrentKm.Name = "textBoxCurrentKm";
+            textBoxCurrentKm.Size = new Size(303, 27);
+            textBoxCurrentKm.TabIndex = 3;
+            textBoxCurrentKm.Validating += textBoxCurrentKm_Validating;
+            // 
+            // buttonCalculate
+            // 
+            buttonCalculate.Location = new Point(40, 281);
+            buttonCalculate.Name = "buttonCalculate";
+            buttonCalculate.Size = new Size(96, 45);
+            buttonCalculate.TabIndex = 7;
+            buttonCalculate.Text = "Пресметни";
+            buttonCalculate.UseVisualStyleBackColor = true;
+            buttonCalculate.Click += buttonCalculate_Click;
+            // 
+            // buttonSave
+            // 
+            buttonSave.Location = new Point(142, 281);
+            buttonSave.Name = "buttonSave";
+            buttonSave.Size = new Size(96, 45);
+            buttonSave.TabIndex = 8;
+            buttonSave.Text = "Запази";
+            buttonSave.UseVisualStyleBackColor = true;
+            buttonSave.Click += buttonSave_Click;
+            // 
+            // checkBoxOil
+            // 
+            checkBoxOil.AutoSize = true;
+            checkBoxOil.Location = new Point(40, 96);
+            checkBoxOil.Name = "checkBoxOil";
+            checkBoxOil.Size = new Size(212, 24);
+            checkBoxOil.TabIndex = 10;
+            checkBoxOil.Text = "Смяна на масло и филтри";
+            checkBoxOil.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGearbox
+            // 
+            checkBoxGearbox.AutoSize = true;
+            checkBoxGearbox.Location = new Point(40, 126);
+            checkBoxGearbox.Name = "checkBoxGearbox";
+            checkBoxGearbox.Size = new Size(281, 24);
+            checkBoxGearbox.TabIndex = 11;
+            checkBoxGearbox.Text = "Смяна на масло на скоростна кутия";
+            checkBoxGearbox.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxTimingBelt
+            // 
+            checkBoxTimingBelt.AutoSize = true;
+            checkBoxTimingBelt.Location = new Point(40, 156);
+            checkBoxTimingBelt.Name = "checkBoxTimingBelt";
+            checkBoxTimingBelt.Size = new Size(303, 24);
+            checkBoxTimingBelt.TabIndex = 12;
+            checkBoxTimingBelt.Text = "Смяна на комплект ангренажен ремък";
+            checkBoxTimingBelt.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPads
+            // 
+            checkBoxPads.AutoSize = true;
+            checkBoxPads.Location = new Point(40, 186);
+            checkBoxPads.Name = "checkBoxPads";
+            checkBoxPads.Size = new Size(165, 24);
+            checkBoxPads.TabIndex = 13;
+            checkBoxPads.Text = "Смяна на накладки";
+            checkBoxPads.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxBrakesDiscs
+            // 
+            checkBoxBrakesDiscs.AutoSize = true;
+            checkBoxBrakesDiscs.Location = new Point(40, 216);
+            checkBoxBrakesDiscs.Name = "checkBoxBrakesDiscs";
+            checkBoxBrakesDiscs.Size = new Size(229, 24);
+            checkBoxBrakesDiscs.TabIndex = 14;
+            checkBoxBrakesDiscs.Text = "Смяна на спирачни дискове";
+            checkBoxBrakesDiscs.UseVisualStyleBackColor = true;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(247, 281);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(96, 45);
+            buttonClear.TabIndex = 15;
+            buttonClear.Text = "Изчисти";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // richTextBoxOutput
+            // 
+            richTextBoxOutput.Location = new Point(369, 96);
+            richTextBoxOutput.Name = "richTextBoxOutput";
+            richTextBoxOutput.ReadOnly = true;
+            richTextBoxOutput.Size = new Size(303, 230);
+            richTextBoxOutput.TabIndex = 16;
+            richTextBoxOutput.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(692, 354);
+            Controls.Add(richTextBoxOutput);
+            Controls.Add(buttonClear);
+            Controls.Add(checkBoxBrakesDiscs);
+            Controls.Add(checkBoxPads);
+            Controls.Add(checkBoxTimingBelt);
+            Controls.Add(checkBoxGearbox);
+            Controls.Add(checkBoxOil);
+            Controls.Add(buttonSave);
+            Controls.Add(buttonCalculate);
+            Controls.Add(textBoxCurrentKm);
+            Controls.Add(textBoxLastChange);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximumSize = new Size(710, 401);
+            MinimumSize = new Size(710, 401);
+            Name = "Form1";
+            Text = " Навременна поддръжка на кола";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxLastChange;
+        private TextBox textBoxCurrentKm;
+        private Button buttonCalculate;
+        private Button buttonSave;
+        private CheckBox checkBoxOil;
+        private CheckBox checkBoxGearbox;
+        private CheckBox checkBoxTimingBelt;
+        private CheckBox checkBoxPads;
+        private CheckBox checkBoxBrakesDiscs;
+        private Button buttonClear;
+        const string MyFile = "CalculatingHistory.rtf";
+        private RichTextBox richTextBoxOutput;
+    }
+}
---
+++ Car-Мaintenance/VSP_238sz_MyProject/Form1.cs
@@ -0,0 +1,105 @@
+﻿namespace VSP_238sz_MyProject
+{
+    public partial class Form1 : Form
+    {
+        private const int OilChange = 10000;
+        private const int gearboxOilChange = 60000;
+        private const int timingBeltKitChange = 80000;
+        private const int padsChange = 50000;
+        private const int brakeDiscsChange = 100000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            string output = "";
+
+            if (this.checkBoxOil.Checked)
+            {
+                output = $"Следващата смяна на масло и филтри трябва да се извърши на {CalculateFutureChange(OilChange)} км, а до тогава остават {CalculateKmLeftToNextChange(OilChange)} км.";
+
+                this.richTextBoxOutput.Text = output;
+
+                this.checkBoxOil.Checked = false;
+            }
+            else if (this.checkBoxGearbox.Checked)
+            {
+                output = $"Следващата смяна на маслото на скоростната кутия трябва да се извърши на {CalculateFutureChange(gearboxOilChange)} км, а до тогава остават {CalculateKmLeftToNextChange(gearboxOilChange)} км.";
+
+                this.richTextBoxOutput.Text = output;
+
+                this.checkBoxGearbox.Checked = false;
+            }
+            else if (this.checkBoxTimingBelt.Checked)
+            {
+                output = $"Следващата смяна на комплект ангренажен ремък трябва да се извърши на {CalculateFutureChange(timingBeltKitChange)} км, а до тогава остават {CalculateKmLeftToNextChange(timingBeltKitChange)} км.";
+
+                this.richTextBoxOutput.Text = output;
+
+                this.checkBoxTimingBelt.Checked = false;
+            }
+            else if (this.checkBoxPads.Checked)
+            {
+                output = $"Следващата смяна на накладките трябва да се извърши на {CalculateFutureChange(padsChange)} км, а до тогава остават {CalculateKmLeftToNextChange(padsChange)} км.";
+
+                this.richTextBoxOutput.Text = output;
+
+                this.checkBoxPads.Checked = false;
+            }
+            else if (this.checkBoxBrakesDiscs.Checked)
+            {
+                output = $"Следващата смяна на спирачните дискове трябва да се извърши на {CalculateFutureChange(brakeDiscsChange)} км, а до тогава остават {CalculateKmLeftToNextChange(brakeDiscsChange)} км.";
+
+                this.richTextBoxOutput.Text = output;
+
+                this.checkBoxBrakesDiscs.Checked = false;
+            }
+        }
+
+       
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            this.textBoxLastChange.Text = "";
+            this.textBoxCurrentKm.Text = "";
+            this.richTextBoxOutput.Text = "";
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            this.richTextBoxOutput.SaveFile(MyFile);
+            MessageBox.Show($"Информацията е запазена успешно в {MyFile}.");
+        }
+
+        private int CalculateFutureChange(int partToChange)
+        {
+            int lastChange = int.Parse(this.textBoxLastChange.Text);
+
+            int futureChange = lastChange + partToChange;
+
+            return futureChange;
+        }
+
+        private int CalculateKmLeftToNextChange(int partToChange)
+        {
+            int currentKm = int.Parse(this.textBoxCurrentKm.Text);
+
+            int kmLeft = CalculateFutureChange(partToChange) - currentKm;
+
+            return kmLeft;
+        }
+
+        private void textBoxCurrentKm_Validating(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            int lastChange = int.Parse(this.textBoxLastChange.Text); 
+            int currentKm = int.Parse(this.textBoxCurrentKm.Text);
+
+            if (currentKm < lastChange)
+            {
+                throw new ArgumentOutOfRangeException("Текущите километри няма как да са по - малко от километрите при последната смяна!");
+            }
+        }
+    }
+}
